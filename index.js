8c679bb20d48520764dc27499b8a519ed432e324
Sean Altemose <sean@seanaltemose.com>
2019-06-17 16:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  handlebarsSetup();
+  initForm();
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  var recipe = getRecipeVals();
+
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+}
