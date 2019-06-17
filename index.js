4880b7a4ddfc76bf752105205b1566a165b1e125
Sean Altemose <sean@seanaltemose.com>
2019-06-17 16:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function createRecipe() {
   var recipe = getRecipeVals();
 
-  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML;
+
+  var template = Handlebars.compile(recipeTemplate);
+  document.getElementById('main').innerHTML = template(recipe);
+}
+
+function updateRecipe() {
+  var recipe = getRecipeVals();
+  var recipteTemplate = document.getElementById("recipe-template").innerHTML;
+  var template = Handlebars.compile(recipeTemplate);
+  document.getElementById("main").innerHTML = template(recipe);
+}
+
+function displayEditForm() {
+  var name = document.getElementById("nameHeader").innerHTML;
+  var description = document.getElementById("recipeDescription").innerHTML;
+  var ingredientNodes = document.getElementsByName("ingredientsList");
+  var ingredients = []
+  for (var i=0; i<ingredientNodes.length; i++){
+    ingredients.push(ingredientNodes[i].innerText);
+  }
+  var recipe = {name, description, ingredients, submitAction: 'createRecipe()'};
+  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML;
+  var template = Handlebars.compile(recipeFormTemplate);
+  document.getElementById("main").innerHTML = template(recipe);
+
+
 }
