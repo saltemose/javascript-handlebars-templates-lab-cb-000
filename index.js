edc833b3142455715bcd92f2bfc0e8d841dc124b
Sean Altemose <sean@seanaltemose.com>
2019-06-17 17:28:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function displayEditForm() {
 
 function getRecipeVals() {
   var ingredientNodes = document.getElementsByName("ingredients");
-  var ingredients = []
+  var ingredients = [];
   for (var i=0; i<ingredientNodes.length; i++){
     if(ingredientNodes[i].value !==""){
     ingredients.push(ingredientNodes[i].value);
